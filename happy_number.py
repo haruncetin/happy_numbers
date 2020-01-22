1b99ccfa42bf22a8fb981a14e833cacde913a2c7
HARUN ÇETİN <hcetin01@hotmail.com>
2020-01-22 17:04:07 +0300

    Create happy_number.py

---
+++ happy_number.py
@@ -0,0 +1,18 @@
+def is_happy(num):
+    while num > 6: # 1'den sonraki en küçük mutlu sayı 7
+        result = 0
+        digits = []
+        temp = num
+        while(temp>0):
+            digits.append(temp%10)
+            temp = temp // 10
+        while(digits != []):
+            digit = digits.pop()
+            result = result + digit * digit
+        num = result
+
+    return num==1
+
+for num in range(99999):
+    if is_happy(num):
+        print(num , "is a HAPPY number!")
